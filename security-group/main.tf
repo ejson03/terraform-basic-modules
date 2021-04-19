84d39b356076b10a40e64f61485d82aa14da00c7
ejson03 <elvisejsondsouza57@gmail.com>
2021-04-19 23:15:37 +0530

    added dyanmic blocks to sg

--- security-group/main.tf
+++ security-group/main.tf
@@ -1,66 +1,33 @@
-# resource "aws_security_group" "sg" {
-#   name   = "project-sg"
-#   vpc_id = var.vpc_id
-#   tags = {
-#     Name = "project-allowance"
-#   }
-# }
+resource "aws_security_group" "this" {
+  count  = var.manage_security_group ? 1 : 0
+  vpc_id = var.vpc_id
 
-# resource "aws_security_group_rule" "inbound_ssh" {
-#   from_port         = 22
-#   protocol          = "tcp"
-#   security_group_id = aws_security_group.sg.id
-#   to_port           = 22
-#   type              = "ingress"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
-
-# resource "aws_security_group_rule" "inbound_http" {
-#   from_port         = 80
-#   protocol          = "tcp"
-#   security_group_id = aws_security_group.sg.id
-#   to_port           = 80
-#   type              = "ingress"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
-
-# resource "aws_security_group_rule" "outbound_all" {
-#   from_port         = 0
-#   protocol          = "-1"
-#   security_group_id = aws_security_group.sg.id
-#   to_port           = 0
-#   type              = "egress"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
-
-resource "aws_security_group" "sg" {
-  name        = "project_allowance_traffic"
-  description = "Allow Web inbound traffic"
-  vpc_id      =  var.vpc_id
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.security_group_ingress
+    content {
+      description      = lookup(ingress.value, "description", null)
+      from_port        = lookup(ingress.value, "from_port", 0)
+      to_port          = lookup(ingress.value, "to_port", 0)
+      protocol         = lookup(ingress.value, "protocol", "-1")
+      cidr_blocks      = compact(split(",", lookup(ingress.value, "cidr_blocks", "")))
+    }
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "egress" {
+    for_each = var.security_group_egress
+    content {
+      description      = lookup(egress.value, "description", null)
+      from_port        = lookup(egress.value, "from_port", 0)
+      to_port          = lookup(egress.value, "to_port", 0)
+      protocol         = lookup(egress.value, "protocol", "-1")
+      cidr_blocks      = compact(split(",", lookup(egress.value, "cidr_blocks", "")))
+    }
   }
 
-  tags = {
-    Name = "project-allowance"
-  }
+  tags = merge(
+    {
+      "Name" : format("%s", var.security_group_name)
+    },
+    var.tags,
+  )
 }
--- security-group/variables.tf
+++ security-group/variables.tf
@@ -1,3 +1,36 @@
 variable "vpc_id" {
-  
+  description = "ID of vpc created"
+  type        = string
+  default     = ""
 }
+
+variable "manage_security_group" {
+  description = "Should be true to adopt and manage security group"
+  type        = bool
+  default     = false
+}
+
+variable "security_group_name" {
+  description = "Name to be used on the security group"
+  type        = string
+  default     = "default"
+}
+
+variable "security_group_ingress" {
+  description = "List of maps of ingress rules to set on the security group"
+  type        = list(map(string))
+  default     = null
+}
+
+variable "security_group_egress" {
+  description = "List of maps of egress rules to set on the security group"
+  type        = list(map(string))
+  default     = null
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
